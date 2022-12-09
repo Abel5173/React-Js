ceea09a83f9b4c9bb3ce40c17008cb6bf19b3316
Abel <abelzeleke5173@gmail.com>
2022-12-09 11:33:53 +0300

    React Hooks

--- my-hooks/src/App.js
+++ my-hooks/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UseEffect from './Components/UseEffect';
 import UseState from './Components/UseState';
 
 function App() {
   return (
-    <UseState/>  
+    // <UseState/>  
+    <UseEffect />
   );
 }
 
