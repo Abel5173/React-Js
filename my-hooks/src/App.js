b0e34eb166667ae4fa263888989168391bb26e1e
Abel <abelzeleke5173@gmail.com>
2022-12-09 15:38:04 +0300

    React useEffect Hooks

--- my-hooks/src/App.js
+++ my-hooks/src/App.js
@@ -1,7 +1,7 @@
-import { Component } from 'react';
 import './App.css';
 import UseEffect from './Components/UseEffect';
 import UseState from './Components/UseState';
+import Component from './Components/Component';
 
 function App() {
   return (
