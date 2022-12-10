95bd5d3b453ebbe00923b00c373f219d58d7be48
Abel <abelzeleke5173@gmail.com>
2022-12-10 14:06:41 +0300

    React tutorial

--- my-react-tutorial/src/App.js
+++ my-react-tutorial/src/App.js
@@ -1,11 +1,12 @@
 import './Components/style.css';
-import ReactEvents from './Components/ReactEvents';
+// import ReactEvents from './Components/ReactEvents';
 import ReactConditionals from './Components/ReactConditionals'
 
+const cars = []
 function App() {
   return (
     // <ReactEvents />
-    <ReactConditionals isGoal = {true}/>
+    <ReactConditionals cars={cars}/>
   );
 }
 
--- my-react-tutorial/src/Components/ReactConditionals.js
+++ my-react-tutorial/src/Components/ReactConditionals.js
@@ -1,17 +1,29 @@
 import React from "react";
 
-function MissedGoal(){
-    return <h1>MISSED</h1>
-}
+// function MissedGoal(){
+//     return <h1>MISSED</h1>
+// }
 
-function MadeGoal(){
-    return <h1>GOAL!</h1>
-}
+// function MadeGoal(){
+//     return <h1>GOAL!</h1>
+// }
 
 export default function ReactConditional(props){
-    const isGoal = props.isGoal
-    if (isGoal){
-        return <MadeGoal />
-    }
-    return <MissedGoal />
+    // const isGoal = props.isGoal
+    // if (isGoal){
+    //     return <MadeGoal />
+    // }
+    // return <MissedGoal />
+
+    const cars = props.cars;
+    return (
+        <>
+            <h1>Garage</h1>
+            {cars.length > 0 && 
+                <h2>
+                    You have {cars.length} cars in your garage.
+                </h2>
+            }
+        </>
+    )
 }
