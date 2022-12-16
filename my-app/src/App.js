43e80266a60c4e44a4f5bb23973480466e43e9a9
Abel <abelzeleke5173@gmail.com>
2022-12-16 21:56:00 +0300

    React

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,22 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import './App.css'
-function App() {
-  const [count, setCount] = useState(0);
-  return (
-    <div>
-      <p>You cicked {count} times.</p>
-      <button onClick={()=>setCount(count + 1)}>Click Me!</button>
-    </div>
-  );
+
+
+function App(){
+  const [km, setKm] = useState(1);
+  function handleChange(e){
+    setKm(e.target.value);
+  }
+  function Convert(){
+    return (km/1.609).toFixed(2)
+  }
+
+  return(
+    <>
+      <input onChange={handleChange} placeholder='Enter km'></input>
+      <p>{km} km is {Convert(km)}miles</p>
+    </>
+  )
 }
 
-export default App;
+export default App
