29276eeb8388580f43fe71e789b4ff9ee327e9da
Abel <abelzeleke5173@gmail.com>
2022-12-24 11:10:00 +0300

    useState react hook practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,19 +3,20 @@ import './App.css'
 
 
 function App(){
-  const [km, setKm] = useState(1);
-  function handleChange(e){
-    setKm(e.target.value);
-  }
-  function Convert(){
-    return (km/1.609).toFixed(2)
-  }
+  const [km, setKm] = useState(1)
 
+  function handleInput(e){
+    setKm(e.target.value)
+  }
+  
+  function to_miles(){
+    return (km/1.609).toFixed(3)
+  }
   return(
-    <>
-      <input onChange={handleChange} placeholder='Enter km'></input>
-      <p>{km} km is {Convert(km)}miles</p>
-    </>
+    <div>
+      <input placeholder="1" onChange={handleInput}/>
+      <h4>{to_miles(km)} miles</h4>
+    </div>
   )
 }
 
